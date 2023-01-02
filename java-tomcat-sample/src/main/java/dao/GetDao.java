8e7d06afa2c92e92e8872d1bf33b45eacf596f00
Akashs95 <115097402+Akashs95@users.noreply.github.com>
2023-01-02 12:05:40 +0530

    Update GetDao.java

--- java-tomcat-sample/src/main/java/dao/GetDao.java
+++ java-tomcat-sample/src/main/java/dao/GetDao.java
@@ -16,15 +16,15 @@ public class GetDao
 		{
 			Class.forName("com.mysql.jdbc.Driver");
 			String ssm_parameter_name = "SSM_PARAMETER_NAME";
-			//Connection con = DriverManager.getConnection("jdbc:mysql://database-2023.ctsdvy14yp5u.us-east-1.rds.amazonaws.com:3306/db","admin","12345678");
+			Connection con = DriverManager.getConnection("jdbc:mysql://database-2023.ctsdvy14yp5u.us-east-1.rds.amazonaws.com:3306/db","admin","12345678");
 			 //Connection con = DriverManager.getConnection("jdbc:mysql://endpoint:3306/db","username","DB_PASSWORD_HERE");
-			
+			/*
 			db_username=java ssm.getparameter('db_username');
 			db_password=java ssm.getparameter('db_password');
 			db_hostname=java ssm.getparameter('db_hostname');
 			
 				Connection con = DriverManager.getConnection("jdbc:mysql://db_hostname:3306/db","db_username","db_password");
-		
+			*/
 			Statement st = con.createStatement();
 			// ResultSet rs = st.executeQuery("select * from employees where emp_no=" + emp_no);
 			ResultSet rs = st.executeQuery("SELECT employees.emp_no,employees.first_name, employees.last_name, titles.title, employees.hire_date, titles.from_date, titles.to_date FROM employees left JOIN titles ON employees.emp_no=titles.emp_no where employees.emp_no=" + emp_no);
